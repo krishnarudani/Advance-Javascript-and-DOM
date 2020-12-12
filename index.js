82d98f346dbcc9a045add5c2f5a6b2bb0eb7bd81
krishnarudani <krishnarudani062@gmail.com>
2020-12-12 16:02:05 +0530

    add all sounds with using switch statement

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
-var numberOfdrumsButton=document.querySelectorAll(".drums").length;
+var numberOfdrumsButtons = document.querySelectorAll(".drum").length;
 
-for(var i=0; i<numberOfdrumsButton; i++){
+for(var i=0; i<numberOfdrumsButtons; i++){
 
-  document.querySelectorAll(".drum").[i].addEventListener("click",function(){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
 
-  var audio=new audio("sounds/tom-1.mp3");
-  audio.play();
+  var buttonInnerHTML =  this.innerHTML;
+
+    switch (buttonInnerHTML) {
+      
+      case "w":
+        var audio = new Audio('sounds/crash.mp3');
+        audio.play();
+      break;
+      
+      case "a":
+          var audio = new Audio('sounds/tom-2.mp3');
+          audio.play();
+      break;
+
+        
+      case "d":
+        var audio= new Audio('sounds/tom-3.mp3');
+        audio.play();
+      break;
+        
+        
+      
+      case "s":
+          var audio= new Audio('sounds/tom-4.mp3');
+          audio.play();
+      break;
+
+      
+      case "j":
+            var audio= new Audio('sounds/tom-1.mp3');
+            audio.play();
+      break;
+
+
+        
+      case "k":
+         var audio= new Audio('sounds/kick-bass.mp3');
+          audio.play();
+      break;
+    
+
+      case "l":
+        var audio = new Audio('sounds/snare.mp3');
+        audio.play();
+      break;
+
+      default: console.log("buttonInnerHTML");
+        
+
+      
+    }
+  
 
   });
-
+  
 }
-
 
 
