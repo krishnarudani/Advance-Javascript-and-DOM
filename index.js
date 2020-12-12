8a3ce07ccff8e37b13dfbbc4b9960489f76d2c7a
krishnarudani <krishnarudani062@gmail.com>
2020-12-12 15:07:22 +0530

    add background-images in button

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-document.querySelector(".set").addEventListener("click",function(){
+var numberOfdrumsButton=document.querySelectorAll(".drums").length;
+
+for(var i=0; i<numberOfdrumsButton; i++){
+
+  document.querySelectorAll(".drum").[i].addEventListener("click",function(){
 
   var audio=new audio("sounds/tom-1.mp3");
   audio.play();
 
-});
+  });
+
+}
+
+
+
