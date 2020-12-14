a99ffc7effcb572465cd9cbcb4b5d93a337f30dd
krishnarudani <krishnarudani062@gmail.com>
2020-12-14 17:58:04 +0530

    found a solution to replay with non blocking

--- index.js
+++ index.js
@@ -2,85 +2,70 @@
 
 var numberOfdrumsButtons = document.querySelectorAll(".drum").length;
 
-for(var i=0; i<numberOfdrumsButtons; i++){
-
-  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-
-  var buttonInnerHTML =  this.innerHTML;
+for (var i = 0; i < numberOfdrumsButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
     makesound(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
   });
-
 }
- 
+var w = new Audio("sounds/tom-1m.mp3");
+var a = new Audio("sounds/tom-2.mp3");
+var d = new Audio("sounds/tom-3.mp3");
+var s = new Audio("sounds/tom-4.mp3");
+var j = new Audio("sounds/crash.mp3");
+var k = new Audio("sounds/kick-bass.mp3");
+var l = new Audio("sounds/snare.mp3");
+
 //detecting keybord press
 
-    document.addEventListener("keypress",function(event){
-     makesound(event.key);
-     buttonAnimation(event.key);
-
+document.addEventListener("keypress", function (event) {
+  makesound(event.key);
+  buttonAnimation(event.key);
 });
 
-    function makesound(key){
+function makesound(key) {
+  switch (key) {
+    case "w":
+      w.currentTime = 0;
+      w.play();
+      break;
 
-      switch (key) {
-      
-        case "w":
-          var audio = new Audio('sounds/crash.mp3');
-          audio.play();
-        break;
-        
-        case "a":
-            var audio = new Audio('sounds/tom-2.mp3');
-            audio.play();
-        break;
-  
-          
-        case "d":
-          var audio= new Audio('sounds/tom-3.mp3');
-          audio.play();
-        break;
-          
-          
-        
-        case "s":
-            var audio= new Audio('sounds/tom-4.mp3');
-            audio.play();
-        break;
-  
-        
-        case "j":
-              var audio= new Audio('sounds/tom-1.mp3');
-              audio.play();
-        break;
-  
-  
-          
-        case "k":
-           var audio= new Audio('sounds/kick-bass.mp3');
-            audio.play();
-        break;
-      
-  
-        case "l":
-          var audio = new Audio('sounds/snare.mp3');
-          audio.play();
-        break;
-  
-        default: console.log("buttonInnerHTML");
-  
-      }
-    }
-     
-    function buttonAnimation(currentkey){
-     
-     var activeButton = document.querySelector("." + currentkey);
+    case "a":
+      a.play();
+      break;
 
-     activeButton.classList.add("pressed");
+    case "d":
+      d.play();
+      break;
 
-      setTimeout(function(){
-        activeButton.classList.remove("pressed")
-      },100);
-    }
+    case "s":
+      s.play();
+      break;
 
+    case "j":
+      j.play();
+      break;
 
+    case "k":
+      k.play();
+      break;
+
+    case "l":
+      l.play();
+      break;
+
+    default:
+      console.log("buttonInnerHTML");
+  }
+}
+
+function buttonAnimation(currentkey) {
+  var activeButton = document.querySelector("." + currentkey);
+
+  activeButton.classList.add("pressed");
+  var timeOut = currentkey.duration;
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 50);
+}
